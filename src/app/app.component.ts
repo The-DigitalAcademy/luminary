f8755d1db63e6dcf9118f702741def0dcd1fbe36
Tshimologo kgashwane <kgashwanetshimologo@gmail.com>
2025-05-26 15:05:02 +0200

    created homepage structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CategoryComponent } from './main/category/category.component';
+import { ProductsCardComponent } from './main/products-card/products-card.component';
+import { FooterComponent } from './main/footer/footer.component';
+import { SearchComponent } from './main/search/search.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CategoryComponent,ProductsCardComponent, FooterComponent,SearchComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
