399cb2ae993cbb57a9ea714783f1a5867c4daadb
Tshimologo kgashwane <kgashwanetshimologo@gmail.com>
2025-05-27 14:43:06 +0200

    positoned the category and serach component

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -5,10 +5,11 @@ import { HeaderComponent } from '../../components/header/header.component';
 import { ProductsCardComponent } from '../../main/products-card/products-card.component';
 import { FooterComponent } from '../../main/footer/footer.component';
 import { SearchComponent } from '../../main/search/search.component';
+import { ViewproductsComponent } from '../viewproducts/viewproducts.component';
 
 @Component({
   selector: 'app-home-page',
-  imports: [HeaderComponent, CategoryComponent, ProductsCardComponent, FooterComponent, SearchComponent],
+  imports: [HeaderComponent, CategoryComponent, ProductsCardComponent, FooterComponent, SearchComponent,ViewproductsComponent],
   templateUrl: './home-page.component.html',
   styles: ``
 })
