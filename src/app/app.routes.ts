b7239dda6bbb21ec0a3a2900736c2ecda3b63a43
Tshimologo kgashwane <kgashwanetshimologo@gmail.com>
2025-05-27 14:43:06 +0200

    positoned the category and serach component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { LoginComponent } from './pages/login/login.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomePageComponent},
   { path: 'login', component: LoginComponent},
   { path: '**', redirectTo: 'home', pathMatch: 'full' }
