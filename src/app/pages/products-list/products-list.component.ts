f5063382c884b6ec07c8b05ebe0b4a03fc090e73
Genesis-ct <kgashwanetshimologo@gmail.com>
2025-05-28 17:14:30 +0200

    created products component

---
+++ src/app/pages/products-list/products-list.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../../services/product.service';
+import { ProductCardComponent } from './product-card/product-card.component';
+import { Product } from '../../models/product.model';
+
+@Component({
+  selector: 'app-products-list',
+  template: `
+    <div class="p-8 grid grid-cols-3 gap-4">
+      @for (product of products; track product.id) {
+      <app-product-card [product]="product" />
+      }
+    </div>
+  `,
+  imports: [ProductCardComponent],
+})
+export class ProductsListComponent implements OnInit {
+  products: Product[] = [];
+
+  constructor(private productService: ProductService) {}
+
+  ngOnInit(): void {
+    this.productService.getProducts().subscribe(
+      (data) => {
+        console.log('Fetched products:', data);
+        this.products = data;
+      },
+      (error) => console.error('Error fetching products:', error)
+    );
+  }
+}
