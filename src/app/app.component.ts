82e582aee2a8d09bbfa0af99a73a44a841420d9c
Genesis-ct <kgashwanetshimologo@gmail.com>
2025-05-28 17:22:11 +0200

    updated app component file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { CategoryComponent } from './main/category/category.component';
-import { ProductsCardComponent } from './main/products-card/products-card.component';
-import { FooterComponent } from './main/footer/footer.component';
-import { SearchComponent } from './main/search/search.component';
 import { HeaderComponent } from './components/header/header.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, CategoryComponent, ProductsCardComponent, FooterComponent, SearchComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [RouterOutlet, HeaderComponent],
+  template: `
+    <app-header />
+    <router-outlet />
+  `,
+  styles: [],
 })
-export class AppComponent {
-  title = 'luminary-app';
-}
+export class AppComponent {}
