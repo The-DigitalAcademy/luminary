8b82b01d55390ab658678188a29d0dc1723cacf8
umkhulubhungane <admin@MacBook-Pro-13.local>
2025-05-28 20:19:50 +0200

    removed none used components from template

--- src/app/components/primary-button/primary-button.component.ts
+++ src/app/components/primary-button/primary-button.component.ts
@@ -3,16 +3,14 @@ import { Component, input, output } from '@angular/core';
 @Component({
   selector: 'app-primary-button',
   imports: [],
-  template: 
-    <button
+  template: `<button
       class=" bg-blue-500 text-white w-full border px-5 py-2 rounded-xl shadow-md hover:opacity-90"
-      (click)="btnClicked.emit()"
-    >
+      (click)="btnClicked.emit()">
       <span class="text-md"> <i class="bi bi-cart2"></i>{{ label() }}</span>
-    </button>
-  ,
+    </button>`,
   styles: ``,
 })
+  
 export class PrimaryButtonComponent {
   label = input<string>();
 
