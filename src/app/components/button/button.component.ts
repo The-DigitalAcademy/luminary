7d8fcfe63e42edfe23035a1d5e3732256568b2c9
radebeneo <radebeneo17@gmail.com>
2025-06-01 16:52:06 +0200

    Fixed button components in the cart page component

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -1,21 +1,20 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, input, output } from '@angular/core';
 
 @Component({
   selector: 'app-button',
   imports: [],
   template: `
     <button
-      class=" text-slate-800 px-2 py-1.5 rounded-lg transition-all"
-      (click)="onClick.emit($event)"
+      class=" text-black w-full px-5 py-2 rounded-xl shadow-md hover:hover:bg-red-500 hover:text-white transition-all"
+      (click)="btnClicked.emit()"
     >
-      <span class="text-md">{{label}}</span>
+      <span class="text-md">{{ label() }}</span>
     </button>
   `,
   styles: ``,
 })
 export class ButtonComponent {
-  @Input() label = '';
-  @Input() type: 'button' | 'submit' | 'reset' = 'button';
-  @Output() onClick = new EventEmitter<Event>();
+  label = input<string>();
 
+  btnClicked = output();
 }
